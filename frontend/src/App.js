a9dc1d1bca63e3a455d1c3157a7b27eebf018887
Siarhei Pats <pats.siarhei@gmail.com>
2022-11-08 18:28:45 -0500

    Added Bootstrap to the frontend app. Added additional application pages.
    Added latest version of bootstrap to the frontend and included reference to bootstrap libraries in the index.js file. Created signup and dashboard pages templates and referenced them in the app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 // Pages and components
 import Home from './pages/home'
+import Signup from './pages/signup';
+import Dashboard from './pages/dashboard'
 
 function App() {
   return (
@@ -12,6 +14,14 @@ function App() {
             <Route
               path = '/'
               element={<Home/>}
+            />
+            <Route
+              path = '/signup'
+              element={<Signup/>}
+            />
+            <Route
+              path = '/dashboard'
+              element={<Dashboard/>}
             />
           </Routes>
         </div>
