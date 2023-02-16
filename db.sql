0648555ad01c52c9716159e7e973f7b2d7e0a0c5
seanyangx <98291896+seanyangx@users.noreply.github.com>
2023-02-15 21:18:22 -0800

    Added all entities

--- db.sql
+++ db.sql
@@ -12,9 +12,30 @@ CREATE TABLE `opm`.`Workspaces` (
   `workspaceId` INT AUTO_INCREMENT,
   `workspaceName` VARCHAR(45) NULL,
   `dateCreated` VARCHAR(45) NULL,
-  PRIMARY KEY (`workspaceId`)
+  `teamId` INT NOT NULL,
+  PRIMARY KEY (`workspaceId`),
+  FOREIGN KEY (`teamId`) REFERENCES `Teams` (`teamId`)
 );
 
+CREATE TABLE `opm`. `Teams` (
+	`teamId` int NOT NULL AUTO_INCREMENT,
+    `teamName` varchar(255) NULL,
+	PRIMARY KEY (`teamId`)
+);
+CREATE TABLE CustomerTeams (
+	customerTeamsId INT AUTO_INCREMENT,
+    customerId INT NOT NULL,
+    teamId INT NOT NULL,
+    PRIMARY KEY (customerTeamsId),
+    FOREIGN KEY (customerId)
+		REFERENCES Customers (customerId)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (teamId)
+		REFERENCES Teams (teamId)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
 CREATE TABLE CustomerWorkspaces (
 	customerWorkspacesId INT AUTO_INCREMENT,
     customerId INT NOT NULL,
@@ -28,4 +49,23 @@ CREATE TABLE CustomerWorkspaces (
 		REFERENCES Workspaces (workspaceId)
         ON DELETE CASCADE
         ON UPDATE CASCADE
+);
+
+CREATE TABLE `opm`.`Boards` (
+	`boardId` int NOT NULL AUTO_INCREMENT,
+	`workspaceId` int NOT NULL, 
+	`boardName` varchar(255) NULL,
+	`boardDescription` text(65535) NULL,
+	PRIMARY KEY (`boardId`),
+	FOREIGN KEY (`workspaceId`) REFERENCES `Workspaces` (`workspaceId`)
+);
+
+CREATE TABLE `opm`.`Tasks` (
+	`taskId` int NOT NULL AUTO_INCREMENT,
+	`boardId` int NOT NULL, 
+	`taskName` varchar(255),
+	`taskInfo` text(65535),
+	`taskDueDate` datetime DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`taskId`),
+	FOREIGN KEY (`boardId`) REFERENCES `Boards` (`boardId`)
 );
