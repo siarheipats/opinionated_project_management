c394a101d11766dc1f57670d94e62e9809ac3049
adabz <adaab@oregonstate.edu>
2023-02-19 14:01:05 -0800

    taskModel.js: filling methods for taskModel.

--- backend/models/taskModel.js
+++ backend/models/taskModel.js
@@ -37,9 +37,59 @@ const Tasks = sequelize.define("Tasks", {
     });
 
 
+    async function createTask(boardId, taskName, taskInfo, taskDueDate) {
+      if (!boardId || !taskName || !taskInfo) {
+          throw Error('All fields must be filled')
+      }
+      const board = await Boards.findOne({ where: { boardId: boardId } });
+      if (!board) {
+        throw Error("Invalid board ID");
+      }
+      const Task = await Tasks.create({
+          boardId: boardId,
+          taskName: taskName,
+          taskInfo: taskInfo,
+          taskDueDate: taskDueDate
+      })
+  
+      return Task;
+  }
+  
+  async function getTask(taskId) {
+      const Task = await Tasks.getTask(taskId);
+      return Task;
+  }
+  
+  async function updateTask(taskId) {
+      // if (!columnId || !columnName) {
+      //     throw Error("All fields must be filled.")
+      // }
+  
+      // await Culumns.update({ 
+      //     columnName: columnName,
+      // }, {
+      //     where: {
+      //         columnId: columnId
+      //     }
+      // })
+  }
+  
+  async function deleteTask(taskId) {
+      // if (!columnId || !columnName) {
+      //     throw Error("All fields must be filled.")
+      // }
+  
+      // // associate tasks with boards and delete tasks with boards?
+  
+      // await Culumns.destroy({
+      //     where: {
+      //         columnId: columnId
+      //     }
+      // })
+  }
 
 
-// exports.createTask = createTask;
-// exports.getTask = getTask;
-// exports.updateTask = updateTask;
-// exports.deleteTask = deleteTask;
+exports.createTask = createTask;
+exports.getTask = getTask;
+exports.updateTask = updateTask;
+exports.deleteTask = deleteTask;
