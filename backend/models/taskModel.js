730a64936ef9028bd852dcbcf871383ace67ff35
adabz <adaab@oregonstate.edu>
2023-02-19 13:26:46 -0800

    taskModel.js: creation and basic Task member items definition.

---
+++ backend/models/taskModel.js
@@ -0,0 +1,37 @@
+const { DataTypes, where } = require('sequelize');
+const { sequelize } = require("../db_connector");
+
+const Tasks = sequelize.define("Tasks", {
+    taskId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    taskName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    boardDescription: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    boardId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'Boards',
+            key: 'boardId'
+        }
+    },
+    taskInfo: {
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    taskDueDate: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+},
+    {
+        timestamps: false
+    });
