1947042572cebcfcadd9c977b416e694dde84d17
seanyangx <98291896+seanyangx@users.noreply.github.com>
2023-03-02 19:31:41 -0800

    Add files via upload

--- frontend/src/components/boardsMain.js
+++ frontend/src/components/boardsMain.js
@@ -1,17 +1,32 @@
 import { React, useEffect, useState } from 'react';
-
+import '../components/board_components/createBoardModal'
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const BoardsMain = () => {
+const BoardsMain = (workspaceId) => {
+    const [boards, setboards] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    useEffect(() => {});
     const theme = createTheme();
+
+    const handleOpenModal = () => {
+        setShowModal(true);
+    }
+    const handleCloseModal = () => {
+        setShowModal(false);
+    }
     return (
         <ThemeProvider theme={theme}>
-            <Button>
+            <Button onClick={handleOpenModal}>
                 <AddIcon />
                 Create Board
             </Button>
+            <CreateBoardModal
+            workspaceId = {workspaceId}
+            showModal = {showModal}
+            handleCloseModalFunction = {handleCloseModel}
+            />
         </ThemeProvider>
     )
 }
