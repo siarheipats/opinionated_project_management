d5fe7a08b00da32ff6e8a2eeb1b1db7f40f12eb9
adabz <adaab@oregonstate.edu>
2023-05-21 20:37:55 -0700

    updates/changes to install scripts.

--- install_linux.sh
+++ install_linux.sh
@@ -1,11 +1,11 @@
 #/usr/bin/bash
+export PATH="/usr/local/bin:$PATH"
 
+# linux 
 # ubuntu installation steps
 sudo apt update
 sudo apt install nodejs
 sudo apt install npm
-sudo apt install mysql-server
-sudo systemctl start mysql.service
 
 # final steps, installing nodejs app packages
 cd frontend/
--- install_mac.sh
+++ install_mac.sh
@@ -1,11 +1,10 @@
 #/usr/bin/bash
+export PATH="/usr/local/bin:$PATH"
 
 # Mac OS X installation steps
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
 brew install node
-brew install mysql
-brew services start mysql
 
 # final steps, installing nodejs app packages
 cd frontend/
